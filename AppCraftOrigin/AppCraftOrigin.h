ccee051adf5d73fcb6d3370a927556ed1a9da17a
Kirill Opekishev <grumpykir@gmail.com>
2019-01-16 15:57:33 +0300

    Initial Commit

---
+++ AppCraftOrigin/AppCraftOrigin.h
@@ -0,0 +1,19 @@
+//
+//  AppCraftOrigin.h
+//  AppCraftOrigin
+//
+//  Created by  Кирилл on 1/16/19.
+//  Copyright © 2019 AppCraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppCraftOrigin.
+FOUNDATION_EXPORT double AppCraftOriginVersionNumber;
+
+//! Project version string for AppCraftOrigin.
+FOUNDATION_EXPORT const unsigned char AppCraftOriginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppCraftOrigin/PublicHeader.h>
+
+
